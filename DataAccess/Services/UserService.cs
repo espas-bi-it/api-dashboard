eab8a15ce5bf7f97ab1d23aaf3c250cb3d5ade38
bi-it-ws <simon.willener@espas.ch>
2024-11-06 21:43:17 +0100

    added sql data to charts

---
+++ DataAccess/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+using DataAccess.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.Services
+{
+    public class UserService
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<UserModel>> GetUsersAsync()
+        {
+            var connectionString = _configuration.GetConnectionString("Default");
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = "SELECT Id, FirstName, LastName FROM [dbo].[user]";
+                return await connection.QueryAsync<UserModel>(query);
+            }
+        }
+    }
+}
