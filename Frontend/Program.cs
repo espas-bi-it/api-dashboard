4b1c7749b89f6f57bbb215f6b25464f009c818bb
bmit-ws <simon.willener@espas.ch>
2024-11-13 16:22:36 +0100

    syncfusion entfernt und forntend neu aufgesetzt

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,39 +1,23 @@
-﻿using DataAccess.Services;
 using Frontend.Components;
-using Frontend.Data;
-using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddSignalR(o => { o.MaximumReceiveMessageSize = 102400000; });
-builder.Services.AddSyncfusionBlazor();
-builder.Services.AddSingleton<PdfService>();
-builder.Services.AddMemoryCache();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-// Add connection string
-var connectionString = builder.Configuration.GetConnectionString("Default");
-
-// Register UserService and CollaboratorService with connection string
-builder.Services.AddTransient<UserService>(provider => new UserService(connectionString));
-builder.Services.AddTransient<CollaboratorService>(provider => new CollaboratorService(connectionString));
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
-
-// Register Syncfusion license
-// Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
